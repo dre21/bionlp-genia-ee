8cec25e5107737da8f11bc284a1a6c27a9a1bb56
Andresta <andre.ramadhan@gmail.com>
2013-09-14 11:55:37 +0900

    use the same key for argument in info

--- bionlp-genia-ee/src/features/FeatureExtraction.py
+++ bionlp-genia-ee/src/features/FeatureExtraction.py
@@ -88,7 +88,7 @@ class FeatureExtraction(object):
                     
                     #print tc, o_sen.words[tc]["string"], "-", p, o_sen.words[p]["string"]                                        
                     feature = self.get_feature(o_sen, tc, p)                    
-                    info = {"doc":o_doc.doc_id, "sen":i, "t":tc, "tp":p}
+                    info = {"doc":o_doc.doc_id, "sen":i, "t":tc, "a":p}
                                         
                     if not o_doc.is_test:
                         label = self.get_tp_label(o_sen, tc, p)  
@@ -126,7 +126,7 @@ class FeatureExtraction(object):
                     
                     #print tc, o_sen.words[tc]["string"], "-", p, o_sen.words[p]["string"]                                        
                     feature = self.get_feature(o_sen, tc, ac)                    
-                    info = {"doc":o_doc.doc_id, "sen":i, "t" : tc, "tt" : ac}
+                    info = {"doc":o_doc.doc_id, "sen":i, "t" : tc, "a" : ac}
                                         
                     if not o_doc.is_test:
                         label = self.get_tt_label(o_sen, tc, ac)  
