4da7ce5d032fb96d2275245ac48628dd68bd40bf
Andresta <andre.ramadhan@gmail.com>
2013-09-17 18:21:13 +0900

    add evaluation for predict dev corpus

--- bionlp-genia-ee/src/genia-ee.py
+++ bionlp-genia-ee/src/genia-ee.py
@@ -6,6 +6,8 @@ Created on Sep 16, 2013
 
 from Prediction import Prediction
 from Learning import Learning
+from sklearn.metrics import precision_recall_fscore_support
+
 
 def learn_dev_train(src, dir_name):
     dict_type = 'mix'
@@ -37,10 +39,37 @@ def predict_dev(src, dir_name):
     dict_type = 'train'
     docs = 'dev'
     
-    prediction = Prediction(src, dir_name, dict_type)
+    """ ----- print validation score for each step ----- """
+    validation = Prediction(src, dir_name, dict_type)
     
+    # validation score for tp    
+    validation.set_prediction_docs(docs, is_test = False)
+    Ypred,Ytest,_ = validation.predict_tp(grid_search = True)
+    print "Gene_expression Transcription Protein_catabolism Phosphorylation Localization Binding"
+    print "Regulation Positive_regulation Negative_regulation"
+    print_matrix(Ytest, Ypred, [1,2,3,4,5,6,7,8,9])
+    
+    # validation score for tt    
+    validation.set_prediction_docs(docs, is_test = False)
+    Ypred,Ytest,_ = validation.predict_tt(grid_search = True)
+    print "Regulation Positive_regulation Negative_regulation"
+    print_matrix(Ytest, Ypred, [7,8,9])
+               
+    
+    """ ----- make a prediction for dev corpus  ----- """
+    prediction = Prediction(src, dir_name, dict_type)    
     prediction.predict(docs)
 
+
+def print_matrix(Ytest, Ypred, labels = None):
+    result = precision_recall_fscore_support(Ytest, Ypred, labels = labels)
+    print "precision"
+    print result[0]
+    print "recall"
+    print result[1]
+    print "f1-score"
+    print result[2]
+    print "\n\n"
 
 if __name__ == '__main__':
     
