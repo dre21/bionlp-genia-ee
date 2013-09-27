6d7c5489efbae5c330a4a2a3a67da822201e40bc
Andresta <andre.ramadhan@gmail.com>
2013-09-27 16:21:23 +0900

    v025

--- bionlp-genia-ee/src/genia-ee.py
+++ bionlp-genia-ee/src/genia-ee.py
@@ -91,8 +91,8 @@ if __name__ == '__main__':
     
     source = "E:/corpus/bionlp2011/project_data"
 
-    dir_name_eval = "test-model-022"    
-    dir_name_final = "model-022"
+    dir_name_eval = "test-model-025"    
+    dir_name_final = "model-025"
     
     # evaluation
     learn_train(source, dir_name_eval)
