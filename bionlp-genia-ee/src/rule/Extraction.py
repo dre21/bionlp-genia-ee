b545d16ddc47861e000bd4fe24671cd633ccef9d
Andresta <andre.ramadhan@gmail.com>
2013-10-18 15:34:01 +0900

    fix extraction

--- bionlp-genia-ee/src/rule/Extraction.py
+++ bionlp-genia-ee/src/rule/Extraction.py
@@ -6,7 +6,7 @@ Created on Oct 3, 2013
 import os, json
 from collections import defaultdict
 from rule.PatternGenerator import PatternGenerator
-from rule.Pattern import TemplatePattern, ExtractionPattern
+from rule.Pattern import TemplatePattern, ExtractionPattern, Pattern
 from model.Dictionary import WordDictionary, TriggerDictionary
 from model.Document import DocumentBuilder
 from corpus.GeniaA2Writer import GeniaA2Writer as A2Writter
@@ -60,11 +60,13 @@ class Extraction(object):
         
         self.extraction_corpus = extraction_corpus
                                 
-        # init pattern generator
+        # init template generator
         self.PG = PatternGenerator(source)
         
         # init a2 writer        
         self.a2 = A2Writter(self.get_out_path(dir_name))
+        
+        self.pattern = Pattern()
     
     def get_out_path(self, dir_name):
         path = os.path.join(source + self.OUT_DIR,dir_name)
@@ -101,7 +103,7 @@ class Extraction(object):
         # document builder
         builder = DocumentBuilder(self.src, word_dict, trigger_dict)
         
-        # load pattern data
+        # load template data
         self.PG.load(self.learning_corpus)
         
         if doc_ids == []:
@@ -113,7 +115,8 @@ class Extraction(object):
             o_doc = self._build_doc(builder, doc_id)            
             
             for i in range(0, len(o_doc.sen)):                                
-            #for i in range(0, 5):
+                print '\nsen:', i
+                print '----------------------------------------'
                 # sentence object
                 o_sen = o_doc.sen[i]
                 # skip sentence without protein
@@ -125,20 +128,7 @@ class Extraction(object):
                 # trigger candidate and arguments
                 tc_list = o_sen.trigger_candidate            
                 arg1_list = tc_list + o_sen.protein
-                arg2_list = list(arg1_list)
-                
-                ''' --------------------------------------------------- '''
-                '''
-                wct = 0
-                for word in o_sen.words:
-                    print wct, word['start'], word['string'], word['score']
-                    wct+=1
-                print o_sen.entity_map
-                print o_sen.trigger_candidate
-                print o_sen.trigger
-                print o_sen.protein
-                '''
-                ''' --------------------------------------------------- '''
+                arg2_list = list(arg1_list)                                
                 
                 # setall tc label
                 for tc in tc_list:
@@ -147,29 +137,72 @@ class Extraction(object):
                     o_sen.words[tc]['type'] = tc_type
                     
                 # for every trigger candidate
-                for tc in tc_list:                    
-                    # pair with every argument 1
+                for tc in tc_list:              
+                    tword = o_sen.words[tc]
                     for arg1 in arg1_list:
-                        # trigger and argument must be different word
                         if tc == arg1: continue
-                        extraction_pattern.append(self._extract_pattern(o_sen, tc, arg1))
-                             
-                        # pair with trigger2  if event is binding or regulations family
-                        if tc_type in self.EVENT_LABEL[5:]:                        
-                            for arg2 in arg2_list:
-                                if tc == arg2:continue
-                                if arg1 == arg2: continue
-                                extraction_pattern.append(self._extract_pattern(o_sen, tc, arg1, arg2))
-                            
-                # filter the extracted pattern in a sentence
-                filered_pattern = self.filter_pattern(extraction_pattern)
-                for p in filered_pattern:
-                    #p.prints()
-                    self.update_non_regulation(o_sen, p)
-                    self.update_regulation(o_sen, p)
-            # write a2                                
-            self.a2.write(o_doc)
+                        # trigger with only 1 argument           
+                        #print tc, arg1             
+                        template, key = self.get_template(o_sen, tc, arg1)
+                        if template != None:
+                            if self.get_pattern_frequency(key) > 20 and template.freq > 4:
+                                print tword['start'], tword['string'], tword['pos_tag'], tword['type'], tc, arg1, key, self.pattern.get_distance_chkdep(o_sen, tc, arg1)
+                                print template.freq, ':',template.pro1_count, template.evt1_count, max(template.dist1), template.prep1
+                                                
+                        for arg2 in arg2_list:                     
+                            #print tc, arg1, arg2           
+                            if tc == arg2 or arg2 == arg1 or tc == arg1 : continue
+                            template, key = self.get_template(o_sen, tc, arg1, arg2)
+                            if template != None:
+                                if self.get_pattern_frequency(key) > 20 and template.freq > 4:
+                                    print tword['start'], tword['string'], tword['pos_tag'], tword['type'],tc, arg1, arg2, key, self.pattern.get_distance_chkdep(o_sen, tc, arg1), self.pattern.get_distance_chkdep(o_sen, tc, arg2)
+                                    print template.freq, ':', template.pro1_count, template.evt1_count, max(template.dist1), template.prep1,  '-', template.pro2_count, template.evt2_count, max(template.dist2), template.prep2 
+                    
                 
+    def get_pattern_frequency(self, key):
+        k = key.split(':')
+        return self.PG.frequency[k[3]+':'+k[2]]
+        
+    
+    def get_template(self, o_sen, t_wn, arg1, arg2 = -1):
+        
+        t_word = o_sen.words[t_wn]
+        
+        if arg2 < 0:
+            pattern, layer, prep1 = self.pattern.get_pattern_1arg(o_sen, t_wn, arg1)
+        else:
+            pattern, layer, prep1, prep2 = self.pattern.get_pattern_2arg(o_sen, t_wn, arg1, arg2)            
+        # build key
+        key = ':'.join([t_word['string'],t_word['pos_tag'], pattern, layer])
+        
+        return self.PG.template.get(key,None), key
+    
+    def pattern_1arg(self, o_sen, t_wn, arg1):
+        prep_string = ''
+        position = {t_wn:'trig', arg1:'arg1'}
+        # word object
+        t_word = o_sen.words[t_wn]
+        t_arg1 = o_sen.words[arg1]
+        
+        if self.pattern.is_chunk(o_sen, t_wn, arg1):            
+            layer = 'chunk'    
+            
+        elif self.pattern.is_phrase(o_sen, t_wn, arg1):                        
+            layer = 'phrase'            
+            # get preposition, if more than 1 get the nearest prep to argument
+            preps = self.pattern.get_prep_word(o_sen,t_wn, arg1)
+            prep_string = preps[-1][0] 
+            prep_wn = preps[-1][1]
+            position[prep_wn] = 'prep1'
+        else:
+            layer = 'clause'
+            
+        # build template type
+        pattern = self.pattern.get_pattern_str(position)        
+        # build key
+        key = ':'.join([t_word['string'],t_word['pos_tag'], pattern, layer])   
+        
+       
     def update_non_regulation(self, o_sen, pattern):
         ttype = o_sen.words[pattern.tc]['type']
         if ttype in self.EVENT_LABEL[6:]: return
@@ -212,7 +245,7 @@ class Extraction(object):
                 
     def _extract_pattern(self, o_sen, tc, arg1, arg2 = -1):
         """
-        extract pattern from a given tc, arg1 and arg2 in a sentence o_sen
+        extract template from a given tc, arg1 and arg2 in a sentence o_sen
         """
         # chunk object
         o_chunk = o_sen.chunk
@@ -220,7 +253,7 @@ class Extraction(object):
         # trigger word object                 
         t_word = o_sen.words[tc]
                 
-        # init extraction pattern
+        # init extraction template
         e_pattern = ExtractionPattern(tc, t_word['string'], t_word['pos_tag'], arg1, arg2)
         e_pattern.arg1_type = o_sen.words[arg1]['type']
         
@@ -231,7 +264,7 @@ class Extraction(object):
         preps1 = self.get_prep_word(o_sen,tc, arg1)
         preps2 = []        
         
-        # make a pattern
+        # make a template
         pattern = {}
         pattern[tc] = 'trg'     
         pattern[arg1] = 'arg1'
@@ -243,7 +276,7 @@ class Extraction(object):
             e_pattern.arg2_type = o_sen.words[arg2]['type']
             # preposition
             preps2 = [p for p in self.get_prep_word(o_sen,tc, arg2) if p not in preps1]            
-            # pattern
+            # template
             pattern[arg2] = 'arg2'            
         
         """ define container type """
@@ -263,7 +296,7 @@ class Extraction(object):
         else:
             e_pattern.container = 'clause'
         
-        # build pattern type
+        # build template type
         pattern_type = ''
         for _,v in sorted(pattern.iteritems()):
             pattern_type += v +'-'        
@@ -274,21 +307,21 @@ class Extraction(object):
                 
     def filter_pattern(self, extraction_patterns):
         """
-        remove pattern in extraction_patterns which are not fulfill the criteria
-        retruns an extracted pattern list
+        remove template in extraction_patterns which are not fulfill the criteria
+        retruns an extracted template list
         """
         pattern_list = {}
                         
         for pattern in extraction_patterns:
             
-            # remove if pattern key is not found in common pattern key
+            # remove if template key is not found in common template key
             if pattern.pattern_type not in self.COMMON_PATTERN_TYPE: continue
             
-            # remove if pattern is not found in template
-            template = self.PG.pattern.get(pattern.get_key(),None)
+            # remove if template is not found in template
+            template = self.PG.template.get(pattern.get_key(),None)
             if template == None: continue
             
-            # remove pattern if frequency of template pattern less than FILTER_FREQUENCY_MIN
+            # remove template if frequency of template template less than FILTER_FREQUENCY_MIN
             if template.freq < self.FILTER_FREQUENCY_MIN: continue
             pattern.freq = template.freq
             
@@ -311,20 +344,20 @@ class Extraction(object):
                 pattern_list[pattern.t_string]['clause'] = []
             
             
-            # add pattern to list
+            # add template to list
             # add patter to container, if container is empty
             if pattern_list[pattern.t_string][pattern.container] == []:
                 pattern_list[pattern.t_string][pattern.container].append(pattern)
             # if container is not empty, compare the frequency
             else:
-                #pattern.prints()
+                #template.prints()
                 if pattern_list[pattern.t_string][pattern.container][0].pattern_type == pattern.pattern_type:
                     pattern_list[pattern.t_string][pattern.container].append(pattern)
                 elif pattern_list[pattern.t_string][pattern.container][0].freq < pattern.freq:
                     pattern_list[pattern.t_string][pattern.container] = [pattern]
         
         filtered_pattern = []        
-        # process pattern for each trigger string
+        # process template for each trigger string
         for string, container in pattern_list.iteritems():
             #print string, len(container['chunk']), len(container['phrase']), len(container['clause'])           
             if container['chunk'] != []:
@@ -358,6 +391,7 @@ class Extraction(object):
                 string_label = label
                 
         if string_label == '':
+            print string
             raise ValueError('Label is not found')
         return string_label
                 
@@ -366,13 +400,13 @@ class Extraction(object):
 if __name__ == '__main__':
     
     source = "E:/corpus/bionlp2011/project_data"
-    learning_corpus = 'train'
+    learning_corpus = 'mix'
     extraction_corpus = 'dev'
     
     dir_name_eval = "rule-test-model-002"    
     dir_name_final = "rule-model-002"
     
-    doc_ids = ['PMID-10090947']
+    doc_ids = ['PMC-2806624-03-RESULTS-02']
 
     WD = WordDictionary(source)    
     WD.load("mix")
@@ -381,7 +415,7 @@ if __name__ == '__main__':
     TD.load("mix")        
 
     extraction = Extraction(source, learning_corpus, extraction_corpus, dir_name_eval)
-    extraction.extract(WD, TD)
+    extraction.extract(WD, TD, doc_ids)
     
         
         
